253f764f0e0cb03690763ed5d481d92033c4c918
park jong ik <pji219@naver.com>
2021-05-04 22:48:50 +0900

    intersection

---
+++ 1-TYPE/1-7-intersection.ts
@@ -0,0 +1,16 @@
+{
+  /**
+   * Intersection Types: AND
+   */
+  type Student = {
+    name: string;
+    score: number;
+  }
+
+  type Worker = {
+    empolyeeId: number;
+    work: () => void;
+  }
+
+  function internWork(person: )
+}
