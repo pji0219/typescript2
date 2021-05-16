3846308c575e33da62844d6531cb4d2079b66190
park jong ik <pji219@naver.com>
2021-05-16 23:12:26 +0900

    intersection

--- 1-TYPE/1-7-intersection.ts
+++ 1-TYPE/1-7-intersection.ts
@@ -5,12 +5,22 @@
   type Student = {
     name: string;
     score: number;
-  }
+  };
 
   type Worker = {
     empolyeeId: number;
     work: () => void;
+  };
+
+  function internWork(person: Student & Worker) {
+    console.log(person.name, person.empolyeeId, person.work());
   }
 
-  function internWork(person: )
+  // 인터섹션 타입을 활용하면 다양한 타입을 묶어서 선언할 수 있다.
+  internWork({
+    name: 'park',
+    score: 100,
+    empolyeeId: 123,
+    work: () => {},
+  });
 }
