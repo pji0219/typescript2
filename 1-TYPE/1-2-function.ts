a9433d94920edaaf227138814031be1a105453a0
pji0219 <pji219@naver.com>
2022-10-10 22:57:18 +0900

    함수

--- 1-TYPE/1-2-function.ts
+++ 1-TYPE/1-2-function.ts
@@ -1,5 +1,3 @@
-// 타입을 쓰면 좀 더 안정적인 프로그래밍이 가능하도록 해주고 타입을 명시함으로써 조금 더 나은 문서화를 하는 효과도 있다.
-
 {
   // JavaScript 💩
   function jsAdd(num1, num2) {
@@ -7,14 +5,14 @@
   }
 
   // TypeScript ✨
-  function add(num1: number, num2: number): number {
+  function Add(num1: number, num2: number): number {
     return num1 + num2;
   }
 
   // JavaScript 💩
   function jsFetchNum(id) {
-    // code ...
-    // code ...
+    // code...
+    // code...
     return new Promise((resolve, reject) => {
       resolve(100);
     });
@@ -29,30 +27,7 @@
     });
   }
 
-  // JavaScript ✨ => TypeScript: 최신 자바스크립트 문법은 타입스크립트에서도 사용 가능
-  // 그리고 자바스크립트에는 아직 포함되지 않은 타입스크립트에서만 제공하는 문법도 사용가능
-
-  // Optional parameter: 선택적으로 파라미터를 받고 싶을 때 사용(인자를 전달해도 되고 안해도 됨)
-  function printName(firstName: string, lastName?: string) {
-    console.log(firstName);
-    console.log(lastName);
-  }
-  printName('jongik', 'park');
-  printName('jongik');
-  printName('Ada', undefined);
-
-  // Default parameter
-  function printMessage(message: string = '무야호~!') {
-    console.log(message);
-  }
-  printMessage();
-
-  // Rest parameter: 인자를 개수 제한 없이 받아오는 파라미터
-  const addNumbers = (...numbers: number[]): number => {
-    return numbers.reduce((a, b) => a + b);
-  };
-
-  console.log(addNumbers(1, 2));
-  console.log(addNumbers(1, 2, 3, 4));
-  console.log(addNumbers(1, 2, 3, 4, 5));
+  //JavaScript ✨ => TypeScript
+  // JS, TS 둘다 사용 가능함
+  // Optional parameter
 }
