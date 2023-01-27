1931f61d9cf1fa12df73bf16245bc198326667db
pji0219 <pji219@naver.com>
2023-01-27 21:34:40 +0900

    abstract2

--- 3-oop/3-8-abstract.ts
+++ 3-oop/3-8-abstract.ts
@@ -1,6 +1,7 @@
 {
   /*
-    
+    absctract: 이 키워드를 사용해서 추상 클래스를 만들어주면 자식 클래스에서 오버라이딩 하지 않고 자식 클래스마다 같은 메서드를
+    다르게 구현 가능
   */
   type CoffeeCup = {
     shots: number;
@@ -12,16 +13,17 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  class CoffeeMachine implements CoffeeMaker {
+  /* 
+    abstract를 사용해 추상 클래스로 해주면 인스턴스를 만들 수 없다(이 클래스를 상속 받는 자식 클래스에서 인스턴스를 생성해야 됨).
+    추상 클래스에서 자식에서 공통적으로 사용해야하는 메서드들이 있다면 여기서 구현하면 되고 자식마다 달라져야하는 메서드는 아래와 같이
+    메서드에도 abstract를 써주면 된다.
+  */
+  abstract class CoffeeMachine implements CoffeeMaker {
     private static BEANS_GRAMM_PER_SHOT: number = 7;
     private coffeeBeans: number = 0;
 
     constructor(coffeeBeans: number) {
       this.coffeeBeans = coffeeBeans;
-    }
-
-    static makeMachine(coffeeBeans: number): CoffeeMachine {
-      return new CoffeeMachine(coffeeBeans);
     }
 
     fillCoffeeBeans(beans: number) {
@@ -50,15 +52,12 @@
       console.log('따듯하게 가열중 입니다.');
     }
 
-    private extract(shots: number): CoffeeCup {
-      console.log(`${shots}샷의 커피를 내리는 중...`);
-
-      return {
-        shots,
-        hasMilk: false,
-        hasSugar: false,
-      };
-    }
+    /* 
+      abstract클래스에서는 이 클래스를 상속 받을 자식 클래스 마다 달라질 수 있는 행동(메서드)이 있다면 abstact 키워드를 붙일 수 있다.
+      자식 클래스에서만 접근 가능하고 외부에서는 직접적으로 접근 못하게 하기 위해 protected 키워드를 사용해준다.
+      자식마다 로직을 다르게 구현해야 되기 때문에 코드는 구현하지 않는다.
+    */
+    protected abstract extract(shots: number): CoffeeCup;
 
     makeCoffee(shots: number): CoffeeCup {
       this.grindBeans(shots);
@@ -76,57 +75,38 @@
       console.log('우유를 가열하는 중 입니다...');
     }
 
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
+    protected extract(shots: number): CoffeeCup {
       this.steamMilk();
-
       return {
-        ...coffee,
+        shots,
         hasMilk: true,
       };
     }
   }
 
+  // constructor의 파라미터가 부모와 같으면 생략해도 됨
   class SweetCoffeeMaker extends CoffeeMachine {
-    constructor(beans: number) {
-      super(beans);
-    }
-
     private insertSugar(): void {
       console.log('설탕을 넣었습니다.');
     }
 
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
+    protected extract(shots: number): CoffeeCup {
       this.insertSugar();
+
       return {
-        ...coffee,
+        shots,
         hasSugar: true,
       };
     }
   }
 
-  // const latteMachine = new CaffeLatteMachine(23, 'SSSS');
-
-  // const coffee = latteMachine.makeCoffee(1);
-  // console.log(coffee);
-  // console.log(latteMachine.serialNumber);
-
-  // const sweetCoffeeMachine = new SweetCoffeeMaker(23);
-  // const sugarCoffee = sweetCoffeeMachine.makeCoffee(1);
-  // console.log(sugarCoffee);
-
   const machines: CoffeeMaker[] = [
-    new CoffeeMachine(16),
     new CaffeLatteMachine(16, '1'),
     new SweetCoffeeMaker(16),
-    new CoffeeMachine(16),
     new CaffeLatteMachine(16, '1'),
     new SweetCoffeeMaker(16),
   ];
 
-  // 다형성의 장점은 여러 클래스가 동일한 한가지의 인터페이스를 구현 하거나 공통된 부모 클래스를 상속 받았을 때
-  // 동일한 메서드를 클래스를 구분하지 않고 아래와 같이 사용할 수 있어서 유용하다.
   machines.forEach((machine) => {
     console.log('-----------------------');
     console.log(machine.makeCoffee(1));
