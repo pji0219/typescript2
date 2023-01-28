3ba6c67c8be17a5bd391d67ace210018409c33bb
pji0219 <pji219@naver.com>
2023-01-29 00:23:58 +0900

    stack 구현

--- 4-oop-projects-quiz/stack.ts
+++ 4-oop-projects-quiz/stack.ts
@@ -15,15 +15,36 @@ type StackNode = {
 // Impl (Implementation): 구현
 class StackImpl implements Stack {
   private _size: number = 0;
+  private head?: StackNode;
+
   get size() {
     return this._size;
   }
 
   push(value: string) {
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
     this._size++;
   }
 
   pop(): string {
+    // null == undefined, null !== undefined
+    if (this.head == null) {
+      throw new Error('Stack is empty!');
+    }
+    const node = this.head;
+    this.head = node.next;
     this._size--;
+
+    return node.value;
   }
+}
+
+const stack = new StackImpl();
+stack.push('pji 1');
+stack.push('kyj 2');
+stack.push('ksh 3');
+
+while (stack.size !== 0) {
+  console.log(stack.pop());
 }
