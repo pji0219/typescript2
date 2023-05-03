86affde3fad3f0beedd53dab7fcd58dc7efeaa09
pji0219 <pji219@naver.com>
2023-05-04 01:33:34 +0900

    type alias와 interface

--- 9-advanced/9-1-interface.ts
+++ 9-advanced/9-1-interface.ts
@@ -18,16 +18,56 @@
   const obj2: PositionInterface = {
     x: 1,
     y: 1,
+    z: 1,
   };
 
   // 둘다 되는 것: class
   class Pos1 implements PositionType {
     x: number;
     y: number;
+
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
   }
 
   class Pos2 implements PositionInterface {
     x: number;
     y: number;
+    z: number;
+
+    constructor(x: number, y: number, z: number) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
   }
+
+  // 둘다 되는 것: extends
+  interface ZPositionInterface extends PositionInterface {
+    z: number;
+  }
+
+  type ZPositionType = PositionType & { z: number };
+
+  // interface만 머지 가능
+  interface PositionInterface {
+    z: number;
+  }
+
+  // type PositionType = {
+  //   z: number;
+  // };
+
+  // type alias만 computed properties 사용 가능
+  type Person = {
+    name: string;
+    age: number;
+  };
+
+  type Name = Person['name']; // string
+
+  type NumberType = number;
+  type Direction = 'left' | 'right';
 }
