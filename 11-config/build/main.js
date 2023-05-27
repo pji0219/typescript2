72beaa8670ca4f51a9886e9dbb90ddff98a5122c
pji0219 <pji219@naver.com>
2023-05-27 23:38:13 +0900

    디버깅

--- 11-config/build/main.js
+++ 11-config/build/main.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('sdf');
+{
+    var Car = /** @class */ (function () {
+        function Car() {
+            this.engine = 0;
+        }
+        Car.prototype.move = function () {
+            var engine = this.engine + 1;
+            console.log('engine bug');
+            console.log(engine);
+        };
+        return Car;
+    }());
+    var car = new Car();
+    car.move();
+}
+//# sourceMappingURL=main.js.map
--- 11-config/src/main.ts
+++ 11-config/src/main.ts
@@ -1,1 +1,13 @@
-console.log('sdf');
+{
+  class Car {
+    engine = 0;
+    move() {
+      const engine = this.engine + 1;
+      console.log('engine bug');
+      console.log(engine);
+    }
+  }
+
+  const car = new Car();
+  car.move();
+}
